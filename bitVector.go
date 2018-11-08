1d536a8a1fca73fecc33554aa5a385ad9760b4fd
ecasdqina <e1711@s.akashi.ac.jp>
2018-11-08 09:26:33 +0900

    DEFINE struct(Builder) && INPLEMENT Builder_methods(NewBuilder, Len, Set, Set0, Set1, Get, Build)

--- bitVector.go
+++ bitVector.go
@@ -27,7 +27,7 @@ func (b BitVector) Len() int {
 	return b.size
 }
 
-// Get returns true / false, the value of the i-th bit in the bit vector.
+// Get returns true or false, the value of the i-th bit in the bit vector.
 func (b BitVector) Get(i int) (bool, error) {
 	if i > b.size {
 		return false, ErrorOutOfRange
@@ -35,7 +35,7 @@ func (b BitVector) Get(i int) (bool, error) {
 	return ((b.v[i/bitLength] >> uint(i%bitLength)) & 1) == 1, nil
 }
 
-// Rank returns the count of 1s / 0s before the i-th bit.
+// Rank returns the count of 1s or 0s before the i-th bit.
 func (b BitVector) Rank(i int, x bool) (int, error) {
 	if x {
 		val, err := b.Rank1(i)
@@ -61,13 +61,75 @@ func (b BitVector) Rank1(i int) (int, error) {
 	return int(b.rank[i/bitLength] + popcount(b.v[i/bitLength] & ^(maskFF<<offset))), nil
 }
 
-// Rank0 return the count of 0x before the i-th bit.
+// Rank0 return the count of 0s before the i-th bit.
 func (b BitVector) Rank0(i int) (int, error) {
 	val, err := b.Rank1(i)
 	if err != nil {
 		return 0, err
 	}
 	return val, nil
+}
+
+// Builder is a builder of BitVector.
+type Builder struct {
+	size int
+	v    []uint64
+}
+
+// NewBuilder makes a new builder of BitVector of the specified size.
+func NewBuilder(size int) *Builder {
+	bufsize := size/bitLength + 1
+
+	return &Builder{
+		size: size,
+		v:    make([]uint64, bufsize),
+	}
+}
+
+// Len returns the size of the bit vector.
+func (b Builder) Len() int {
+	return b.size
+}
+
+// Set sets i-th bit in the bit vector to v.
+func (b *Builder) Set(i int, v bool) {
+	if v {
+		b.v[i/bitLength] |= uint64(1) << uint(i%bitLength)
+	} else {
+		b.v[i/bitLength] &^= (uint64(1) << uint(i%bitLength))
+	}
+}
+
+// Set1 sets i-th bit in the bit vector to 1.
+func (b *Builder) Set1(i int) {
+	b.Set(i, true)
+}
+
+// Set0 sets i-th bit in the bit vector to 0.
+func (b *Builder) Set0(i int) {
+	b.Set(i, false)
+}
+
+// Get returns true or false, i-th bit in the bit vector.
+func (b Builder) Get(i int) bool {
+	return (b.v[i/bitLength] << uint(i%bitLength) & 1) == 1
+}
+
+// Build builds a BitVector from the builder.
+func (b Builder) Build() *BitVector {
+	rank := make([]int, len(b.v))
+	count := 0
+
+	for i, x := range b.v {
+		rank[i] = count
+		count += popcount(x)
+	}
+
+	return &BitVector{
+		size: b.size,
+		v:    b.v,
+		rank: rank,
+	}
 }
 
 func popcount(x uint64) int {
